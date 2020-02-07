008b69c77f5f8035a6dd638fff71677e3e7ddabb
JuWeber99 <s_weberj@stud.hwr-berlin.de>
2020-02-07 16:05:57 +0100

    metadata component in progress

--- src/components/calendar/CalendarMetadata.tsx
+++ src/components/calendar/CalendarMetadata.tsx
@@ -1,26 +1,23 @@
 import React, {useContext} from 'react';
 import {CalendarContext} from "../../model/DateContextProvider";
-import {dummyData} from "../../resources/hwr-wi-b-6";
 import moment from "moment";
 import {MyEvent} from "../../model/EventModel";
 
 const CalendarMetadata = () => {
     const calendarContext = useContext<CalendarContext>(CalendarContext);
-    const tomorrow = calendarContext.state.dateContext.date(calendarContext.state.dateContext.date()).subtract(1, "week");
-    console.log(tomorrow);
-    console.log(calendarContext.state.dateContext.date());
-
-    const semesterStartMoment = moment(dummyData.vcalendar.vevent[0].dtstart);
-    const semesterEndMoment = moment(dummyData.vcalendar.vevent[dummyData.vcalendar.vevent.length - 1].dtend);
+    const tomorrow = calendarContext.dateContext.date(calendarContext.dateContext.date()).subtract(1, "week");
+    const semesterStartMoment = moment(calendarContext.events[0].dtstart);
+    const semesterEndMoment = moment(calendarContext.events[calendarContext.events.length - 1].dtend);
     const semesterDaysLeft = semesterEndMoment.from(semesterStartMoment, true);
 
-    const calculateWeeklyHourse = (eventData: MyEvent[]) => {
-        let weekMinutes = 0;
-        for (let i = 0; i < eventData.length; i++) {
+    const calculateWeeklyHours = (eventData: MyEvent[]): number => {
+        let weekMinutes: number = 0;
+        for (let i = 1; i < eventData.length; i++) {
             if (moment(eventData[i].dtend).isoWeek() === moment(eventData[i - 1].dtstart).isoWeek()) {
-                weekMinutes += moment.duration(moment(dummyData.vcalendar.vevent[0].dtend).diff(dummyData.vcalendar.vevent[0].dtstart)).asMinutes()
+                weekMinutes += moment.duration(moment(calendarContext.events[i - 1].dtend).diff(calendarContext.events[i - 1].dtstart)).asMinutes()
             }
         }
+        return weekMinutes / 60;
     };
 
     return (
@@ -28,7 +25,8 @@ const CalendarMetadata = () => {
             <p>{semesterStartMoment.format("DD.MM.YYYY")}</p>
             <p>{semesterEndMoment.format("DD.MM.YYYY")}</p>
             <p>{semesterDaysLeft}</p>
-            <p>{moment.duration(moment(dummyData.vcalendar.vevent[0].dtend).diff(dummyData.vcalendar.vevent[0].dtstart)).toISOString()}</p>
+            <p>{calculateWeeklyHours(calendarContext.events)}</p>
+            <p>{moment.duration(moment(calendarContext.events[0].dtend).diff(calendarContext.events[0].dtstart)).toISOString()}</p>
         </div>
     );
 };
