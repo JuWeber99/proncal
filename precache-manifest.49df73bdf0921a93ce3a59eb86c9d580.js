92cd39711b61ca92979457e94ce6d93f40be2a37
JuWeber99 <s.weberj@stud.hwr-berlin.de>
2020-02-09 20:00:36 +0100

    Updates

---
+++ precache-manifest.49df73bdf0921a93ce3a59eb86c9d580.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027ae41650b6580b88d811efd57274df",
+    "url": "/proncal/index.html"
+  },
+  {
+    "revision": "d30f69263df2fc082efd",
+    "url": "/proncal/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "c85fbbf7cb70d25dadd3",
+    "url": "/proncal/static/css/main.ff934a69.chunk.css"
+  },
+  {
+    "revision": "d30f69263df2fc082efd",
+    "url": "/proncal/static/js/2.af4525af.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/proncal/static/js/2.af4525af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85fbbf7cb70d25dadd3",
+    "url": "/proncal/static/js/main.1c80d539.chunk.js"
+  },
+  {
+    "revision": "ef3d7f944abe2cdb9766",
+    "url": "/proncal/static/js/runtime-main.8e2600f6.js"
+  },
+  {
+    "revision": "a2c28bb3e2c15a43bfb78e19d4712a85",
+    "url": "/proncal/static/media/bg2.a2c28bb3.jpg"
+  },
+  {
+    "revision": "b033291c20a7e2393c82744c0b015401",
+    "url": "/proncal/static/media/calendar2.b033291c.svg"
+  },
+  {
+    "revision": "244061df689dc8336afb6e0308fb4c45",
+    "url": "/proncal/static/media/hwr-logo-alt.244061df.svg"
+  }
+]);
