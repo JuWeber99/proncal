6cc64af9c131fd7fa242591a47d73d00eb7f9ca1
JuWeber99 <s.weberj@stud.hwr-berlin.de>
2020-02-10 02:59:09 +0100

    deploy

--- src/model/DateContextProvider.tsx
+++ src/model/DateContextProvider.tsx
@@ -108,7 +108,7 @@ export const DateContextProvider: FunctionComponent<{ children: ReactNode }> = (
         async function fetchWetherData() {
             const response: AxiosResponse<DarkSideWether> = await axios.request<DarkSideWether>({
                 method: "GET",
-                url: "https://api.darksky.net/forecast/343201781aa363c6c7b72159954bcd8a/52.5,13.41667",
+                url: "http://aaaapi.darksky.net/forecast/343201781aa363c6c7b72159954bcd8a/52.5,13.41667",
             });
             console.log(response);
             setWetherData(response.data)
